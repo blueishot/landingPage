086c11341565abf7288b0bcdcffe9daf8893b823
blueishot <fabricioteixeirahungria@gmail.com>
2021-08-10 14:49:23 -0300

    Footer links and images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,20 +6,20 @@ const Footer = () => {
             <div className='footerList'>
                 <div className='footerColumn'>
                     <span style={{fontWeight:'bolder'}}>Politicas</span>
-                    <span className='spanHover'>Termos de uso</span>
-                    <span className='spanHover'>DMCa notification</span>
+                    <span className='spanHover' onClick={() => window.location.href='https://github.com/blueishot'}>Termos de uso</span>
+                    <span className='spanHover' onClick={() => window.location.href='https://github.com/blueishot'}>DMCa notification</span>
                 </div>
                 <div className='footerColumn'>
                     <span style={{fontWeight:'bolder'}}>Informativos</span>
-                    <span className='spanHover'>Perguntas frequentes</span>
-                    <span className='spanHover'>Recrutamento</span>
-                    <span className='spanHover'>Contato</span>
+                    <span className='spanHover' onClick={() => window.location.href='https://github.com/blueishot'}>Perguntas frequentes</span>
+                    <span className='spanHover' onClick={() => window.location.href='https://github.com/blueishot'}>Recrutamento</span>
+                    <span className='spanHover' onClick={() => window.location.href='https://github.com/blueishot'}>Contato</span>
                 </div>
                 <div className='footerColumn'>
                     <span style={{fontWeight:'bolder'}}>Acesso rapido</span>
-                    <span className='spanHover'>Inicio</span>
-                    <span className='spanHover'>Planos futuros</span>
-                    <span className='spanHover'>Membros da equipe</span>
+                    <span className='spanHover' onClick={() => window.location.href='https://github.com/blueishot'}>Inicio</span>
+                    <span className='spanHover'  onClick={() => window.location.href='https://github.com/blueishot'}>Planos futuros</span>
+                    <span className='spanHover' onClick={() => window.location.href='https://github.com/blueishot'}>Membros da equipe</span>
                 </div>
             </div>
             <div className='footerSite'>
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ import Footer from './Footer.js'
 
 const Home = () => {
     return (
+        window.location.pathname === '/'
+        ?
         <div>
             <Menu />
             <div>
@@ -112,59 +114,59 @@ const Home = () => {
             padding   : 10,
                 }}>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/412656/moviethumb/chaos-walking-603b9bb9102fd.jpg' draggable='none' style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/9428/moviethumb/the-royal-tenenbaums-5834c026495ca.jpg' draggable='none' style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/8367/moviethumb/robin-hood-prince-of-thieves-5861788196265.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/83666/moviethumb/moonrise-kingdom-585980b0dc6d7.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/630004/moviethumb/the-vault-606a2275ae874.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/120467/moviethumb/the-grand-budapest-hotel-5394b64fbc708.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/644583/moviethumb/the-mauritanian-60474e135adef.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/485403/moviethumb/galveston-5c0f7f1188b51.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/458220/moviethumb/palmer-6039bf4796523.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/103/moviethumb/taxi-driver-604eb741bbeb6.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/399566/moviethumb/godzilla-vs-kong-600aafee37802.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/466272/moviethumb/once-upon-a-time-in-hollywood-5c929f22d4aff.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/432799/moviethumb/spoor-5bb16cc0504b5.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/419430/moviethumb/get-out-59191249a8bf6.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/651610/moviethumb/pixie-60fec379818c8.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/22538/moviethumb/scott-pilgrim-vs-the-world-5357ee14dca12.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/675327/moviethumb/shadow-in-the-cloud-5ff6552fc49e0.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/157336/moviethumb/interstellar-5ce1f78a996ac.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/614409/moviethumb/to-all-the-boys-always-and-forever-lara-jean-6051fb51be9a4.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/171274/moviethumb/inherent-vice-552ab8e92fa34.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/346687/moviethumb/the-water-man-60c48fb012c01.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/11104/moviethumb/chungking-express-5429753e3685e.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/497698/moviethumb/black-widow-5e6e8a54b9963.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/635302/moviethumb/demon-slayer--kimetsu-no-yaiba--the-movie-mugen-train-60d46824f0a9c.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/741636/moviethumb/bloodthirsty-6067248a8bbd2.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/550/moviethumb/fight-club-5f26b5dccac27.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                     <SplideSlide>
-                        <img src='https://fanart.tv/fanart/movies/527774/moviethumb/dragon-empire-600c1ec1ac5db.jpg'  style={{width:'100%',
+                        <img src='https://fanart.tv/fanart/movies/301365/moviethumb/the-neon-demon-5e52f615f0a99.jpg'  style={{width:'100%',
                         height:'auto'}} className='filmsStyle'></img>
                     </SplideSlide>
                 </Splide>
@@ -308,6 +310,10 @@ const Home = () => {
                 </Splide>
                 <Footer/>
             </div>
+        </div>
+        :
+        <div style={{display:'flex', alignItems:'center', justifyContent:'center', height:'100vh'}}>
+            <img width='500px' draggable='false' src='https://camo.githubusercontent.com/a90e1dd7aecd6635329133d9172677d4a90850058cbd5eb08cfa46f588357e91/68747470733a2f2f692e696d6775722e636f6d2f6c3451506f71382e6a7067'></img>
         </div>
         
     )
